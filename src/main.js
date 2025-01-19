566936cf7ac771b04a81e37a29eb648c95040ec4
navneetkumaryadav <navneetyadaviitbombay@gmail.com>
2025-01-19 15:16:50 +0530

    App

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import router from "./router"; // Import the router
+import "./registerServiceWorker";
 
 const app = createApp(App);
 app.use(router); // Tell Vue to use the router
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'Letter',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    manifestOptions: {
+      icons: [
+        {
+          src: 'img/icons/favicon.jpg',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'img/icons/favicon.jpg',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  },
 });
